e83b20523a26c74a1c2baef7bf814aad944094c3
Shin Yamaga <shinyamaga@ShinnoMacBook-Air.local>
2022-01-13 20:59:04 +0900

    機能追加

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ def handle_text_message(event):
 def handle_image_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextMessage(''))
+        TextSendMessage('画像ファイルです'))
 
 if __name__ == "__main__":
 #    app.run()
