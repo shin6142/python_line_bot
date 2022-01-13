d10563b13db8b263506d489f6de64c243fce5d5d
Shin Yamaga <shinyamaga@ShinnoMacBook-Air.local>
2022-01-13 21:30:03 +0900

    予定回答機能の修正

--- main.py
+++ main.py
@@ -47,10 +47,6 @@ def handle_text_message(event):
         line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage('どういたしまして'))
-    elif '予定' in event.message.text:
-        line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage('予定を答えます'))
     else:
         line_bot_api.reply_message(
         event.reply_token,
