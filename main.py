e822f3df3c8ec3097758ab34a7286352ab0616ba
Shin Yamaga <shinyamaga@ShinnoMacBook-Air.local>
2022-01-16 08:58:55 +0900

    機能追加

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ app = Flask(__name__)
 #環境変数取得
 YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
 YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
+MY_LINE_ID = os.environ["MY_LINE_ID"]
 
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
@@ -42,7 +43,7 @@ def callback():
 
 @app.route("/<username>")
 def notify(username):
-    user_id = "09065906142"
+    user_id = MY_LINE_ID
     line_bot_api.push_message(user_id, messages=username)
 
 
