5ecc50087a02be0af710d18ae5cd31e93ae0e745
Shin Yamaga <shinyamaga@ShinnoMacBook-Air.local>
2022-01-19 22:05:09 +0900

    js追加

---
+++ statics/test.js
@@ -0,0 +1,38 @@
+const weeks = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT']
+const date = new Date()
+const year = date.getFullYear()
+const month = date.getMonth() + 1
+const startDate = new Date(year, month - 1, 1) // 月の最初の日を取得
+const endDate = new Date(year, month, 0) // 月の最後の日を取得
+const endDayCount = endDate.getDate() // 月の末日
+const startDay = startDate.getDay() // 月の最初の日の曜日を取得
+let dayCount = 1 // 日にちのカウント
+let calendarHtml = '' // HTMLを組み立てる変数
+
+calendarHtml += '<h1>' + year + '/' + month + '</h1>'
+calendarHtml += '<table>'
+
+
+for (let i = 0; i < weeks.length; i++) {
+    calendarHtml += '<td>' + weeks[i] + '</td>'
+}
+
+
+for (let w = 0; w < 6; w++) {
+    calendarHtml += '<tr>'
+
+    for (let d = 0; d < 7; d++) {
+        if (w == 0 && d < startDay) {
+            calendarHtml += '<td></td>'
+        } else if (dayCount > endDayCount) {
+            calendarHtml += '<td></td>'
+        } else {
+            calendarHtml += '<td>' + dayCount + '</td>'
+            dayCount++
+        }
+    }
+    calendarHtml += '<tr>'
+}
+calendarHtml += '</table>'
+
+document.querySelector('#calendar').innerHTML = calendarHtml
