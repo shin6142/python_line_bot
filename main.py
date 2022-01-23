759cd2bcce12d804f24786de1c2c5d9139cd1607
Shin Yamaga <shinyamaga@ShinnoMacBook-Air.local>
2022-01-23 17:44:51 +0900

    templates追加

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, abort, render_template
-
+from importlib_metadata import re
 from linebot import (
     LineBotApi, WebhookHandler
 )
@@ -7,11 +7,12 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage, ImageMessage, ImageSendMessage
+    MessageEvent, TextMessage, TextSendMessage, FollowEvent, TemplateSendMessage, ButtonsTemplate, MessageAction
 )
 from os.path import join, dirname
 from dotenv import load_dotenv
 import os
+from models import model
 
 
 app = Flask(__name__, static_folder='static') 
@@ -19,65 +20,79 @@ app = Flask(__name__, static_folder='static')
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
+
 #環境変数取得
-# YOUR_CHANNEL_ACCESS_TOKEN = os.environ.get("YOUR_CHANNEL_ACCESS_TOKEN")
-# YOUR_CHANNEL_SECRET = os.environ.get("YOUR_CHANNEL_SECRET")
-# MY_LINE_ID = os.environ.get("MY_LINE_ID")
+YOUR_CHANNEL_ACCESS_TOKEN = os.environ.get("YOUR_CHANNEL_ACCESS_TOKEN")
+YOUR_CHANNEL_SECRET = os.environ.get("YOUR_CHANNEL_SECRET")
+MY_LINE_ID = os.environ.get("MY_LINE_ID")
 
-# line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
-# handler = WebhookHandler(YOUR_CHANNEL_SECRET)
+line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(YOUR_CHANNEL_SECRET)
+print(handler)
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+@handler.add(FollowEvent)
+def handle_follow(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text='初めまして')
+    )
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
 
 
-# @app.route("/callback", methods=['POST'])
-# def callback():
-#     # get X-Line-Signature header value
-#     signature = request.headers['X-Line-Signature']
-
-#     # get request body as text
-#     body = request.get_data(as_text=True)
-#     app.logger.info("Request body: " + body)
-
-#     # handle webhook body
-#     try:
-#         handler.handle(body, signature)
-#     except InvalidSignatureError:
-#         abort(400)
-
-#     return 'OK'
-
-
-@app.route("/")
-def show_index():
-    return render_template('index.html')
-
-# @app.route("/<username>")
-# def send_message(username):
-#     if username != 'favicon.ico':
-#         messages = TextSendMessage(text=f'{username}がジムにチェックインしました')
-#         line_bot_api.broadcast(messages=messages)
-#         line_bot_api.push_message(MY_LINE_ID, messages)
-#     img_path = 'images/woman_yoga.svg'
+# @app.route("/", methods=['GET'])
+# def show_index():
 #     return render_template('index.html')
 
-# @handler.add(MessageEvent, message=TextMessage)
-# def handle_text_message(event):
-#     if event.message.text == 'ありがとう':
-#         line_bot_api.reply_message(
-#             event.reply_token,
-#             TextSendMessage('どういたしまして'))
-#     else:
-#         line_bot_api.reply_message(
-#             event.reply_token,
-#             TextSendMessage(text=event.message.text))
-
-# @handler.add(MessageEvent, message=ImageMessage)
-# def handle_image_message(event):
-#     line_bot_api.reply_message(
-#         event.reply_token,
-#         TextSendMessage('いい写真ですね'))
+@app.route("/<username>")
+def send_message(username):
+    if username != 'favicon.ico':
+        messages = TextSendMessage(text=f'{username}がジムにチェックインしました')
+        # line_bot_api.broadcast(messages=messages)
+        line_bot_api.push_message(MY_LINE_ID, messages)
+    return render_template('index.html')
 
 
+@app.route('/', methods=['GET'])
+def register_get():
+	return render_template('register.html', \
+		title = 'Form Sample(get)', \
+		message = '名前を入力して下さい。')
 
+@app.route('/', methods=['POST'])
+def register_post():
+	username = request.form['username']
+	model.add_user(username)
+
+@app.route('/user_detail/<username>')
+def show_user_detail(username):
+    user = model.get_user(username)
+    return render_template('user_detail.html', id=user.id, name=user.username )
+
+@app.route('/<int:user_id>')
+def check_in(user_id):
+    model.add_stamp(user_id)
 
 if __name__ == "__main__":
 #    app.run()
---
+++ models/model.py
@@ -0,0 +1,54 @@
+from flask import Flask, request, abort, render_template
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.sql.functions import current_timestamp
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///calender.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+
+    __tablename__ = 'user'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    stamps = relationship("Stamp")
+
+
+class Stamp(db.Model):
+    
+    __tablename__ = 'stamp'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    created_at = db.Column(db.DateTime, nullable=False, server_default=current_timestamp())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+def add_user(username):
+    if username != 'favicon.ico':
+        new_user = User(username=username)
+        db.session.add(new_user)
+        db.session.commit()
+
+def add_stamp(user_id: int):
+    new_stamp = Stamp(user_id=user_id)
+    db.session.add(new_stamp)
+    db.session.commit()
+
+def get_user(username):
+    user = db.session.query(User).filter(User.username==username).first()
+    return user
+
+# if __name__ == "__main__":
+#     app.run()
+
+
+# db.drop_all()
+# db.create_all()
+
+result = get_user('user1')
+print(result.username)
+# for user in users:
+#     print(user.username, user.id)
